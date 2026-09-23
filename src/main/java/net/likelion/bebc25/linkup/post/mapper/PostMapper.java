52de1a67cbffad4301e9cf1d7e9849b7ca8403c8
GWC1111 <wonil4198@gmail.com>
2026-09-23 17:24:33 +0900

    FEAT: 팔로잉한 게시글 조회

--- src/main/java/net/likelion/bebc25/linkup/post/mapper/PostMapper.java
+++ src/main/java/net/likelion/bebc25/linkup/post/mapper/PostMapper.java
@@ -2,8 +2,11 @@ package net.likelion.bebc25.linkup.post.mapper;
 
 import net.likelion.bebc25.linkup.post.domain.Post;
 import net.likelion.bebc25.linkup.post.dto.PostDetailResponse;
+import net.likelion.bebc25.linkup.post.dto.PostFeedResponse;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface PostMapper {
@@ -12,4 +15,5 @@ public interface PostMapper {
 
     // 게시글 단 건 조회
     Post findById(@Param("postId") Long postId);
+
 }
